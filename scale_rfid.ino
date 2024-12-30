c538105ccb057d4a6f3d9826e81e90dbfc291550
Christian Larsen <christian.larsen93@hotmail.com>
2024-12-30 14:41:56 +0100

    Working example

--- scale_rfid.ino
+++ scale_rfid.ino
@@ -1,10 +1,36 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         22          // Configurable, see typical pin layout above
+#define SS_PIN          21         // Configurable, see typical pin layout above
+// Assuming SPI(VSPI) pins
+// SCK 18
+// MISO 19 
+// MOSI 23
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+	Serial.begin(9600);		// Initialize serial communications with the PC
+	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+	SPI.begin();			// Init SPI bus
+	mfrc522.PCD_Init();		// Init MFRC522
+	delay(4);				// Optional delay. Some board do need more time after init to be ready, see Readme
+	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
+	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("hej!");
-  delay(1000);
+	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+
+	// Select one of the cards
+	if ( ! mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+
+	// Dump debug info about the card; PICC_HaltA() is automatically called
+	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
